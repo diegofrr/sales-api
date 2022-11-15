81b640d2c829a26d2b205192ed935c85c5437377
Diêgo Raian <diego.raian@lavid.ufpb.br>
2022-11-15 14:11:06 -0300

    :rocket: initial super commit

---
+++ src/shared/http/server.ts
@@ -0,0 +1,39 @@
+import 'reflect-metadata';
+import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import routes from './routes';
+import AppError from '@shared/errors/AppError';
+import '@shared/typeorm';
+
+const server = express();
+
+server.use(cors());
+server.use(express.json());
+
+server.use(routes);
+
+server.use(
+    (
+        error: Error,
+        request: Request,
+        response: Response,
+        next: NextFunction,
+    ) => {
+        if (error instanceof AppError) {
+            return response.status(error.statusCode).json({
+                status: 'error',
+                message: error.message,
+            });
+        }
+
+        return response.status(500).json({
+            status: 'error',
+            message: 'Internal server error',
+        });
+    },
+);
+
+server.listen(3333, () => {
+    // eslint-disable-next-line no-console
+    console.log('🟢 Server started on port 3333!');
+});
