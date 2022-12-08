e6f6f00861e57f75396575356ab47af7e1c693f3
Diêgo Ferreira <diegofrr.19@gmail.com>
2022-12-08 13:08:20 -0300

    [feat] add Customers migration

--- src/shared/typeorm/migrations/1670515524934-CreateCustomers.ts
+++ src/shared/typeorm/migrations/1670515524934-CreateCustomers.ts
@@ -1,10 +1,10 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export class CreateUsers1669153301945 implements MigrationInterface {
+export class CreateCustomers1670515524934 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.createTable(
             new Table({
-                name: 'users',
+                name: 'customers',
                 columns: [
                     {
                         name: 'id',
@@ -13,14 +13,14 @@ export class CreateUsers1669153301945 implements MigrationInterface {
                         generationStrategy: 'uuid',
                         default: 'uuid_generate_v4()',
                     },
-                    { name: 'name', type: 'varchar' },
+                    {
+                        name: 'name',
+                        type: 'varchar',
+                    },
                     {
                         name: 'email',
                         type: 'varchar',
-                        isUnique: true,
                     },
-                    { name: 'password', type: 'varchar' },
-                    { name: 'avatar', type: 'varchar', isNullable: true },
                     {
                         name: 'created_at',
                         type: 'timestamp',
@@ -37,6 +37,6 @@ export class CreateUsers1669153301945 implements MigrationInterface {
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropTable('users');
+        await queryRunner.dropTable('customers');
     }
 }
