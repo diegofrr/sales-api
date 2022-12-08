6dfc1e59e3639fa3bc2dbb3b09077b35e351b8a0
Diêgo Ferreira <diegofrr.19@gmail.com>
2022-12-08 14:01:26 -0300

    [feat] add isAuthenticated middleware to Customer routes

--- src/modules/customers/routes/customers.routes.ts
+++ src/modules/customers/routes/customers.routes.ts
@@ -2,9 +2,12 @@ import CustomersController from '../controllers/CustomersController';
 
 import { Router } from 'express';
 import { celebrate, Joi, Segments } from 'celebrate';
+import isAuthenticated from '@shared/http/middlewares/isAuthenticated';
 
 const customersRouter = Router();
 const customersController = new CustomersController();
+
+customersRouter.use(isAuthenticated);
 
 customersRouter.get('/', customersController.index);
 
